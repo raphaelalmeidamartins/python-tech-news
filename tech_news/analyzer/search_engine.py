9a020300e511e7b91660d0b563e1377b70d166ec
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-10 19:58:39 -0300

    :sparkles: feat: search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news_list = search_news(query)
+    return [
+        (news["title"], news["url"])
+        for news in news_list
+    ]
 
 
 # Requisito 7
