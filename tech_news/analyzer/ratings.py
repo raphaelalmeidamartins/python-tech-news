ff49b36ce57f51a1eadf9626a1898ce6fa892100
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-10 20:22:19 -0300

    :sparkles: feat: top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    query = {"$sort": {"comments_count": -1, "$limit": 5}}
+    news_list = search_news(query)
+    return [
+        (news["title"], news["url"])
+        for news in news_list
+    ]
 
 
 # Requisito 11
