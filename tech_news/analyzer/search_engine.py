47e0cf8c2caab259821a3094dae137a21c4f2f94
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-11-10 20:12:02 -0300

    :sparkles: feat: search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -46,4 +46,9 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"category": {"$regex": category, "$options": "i"}}
+    news_list = search_news(query)
+    return [
+        (news["title"], news["url"])
+        for news in news_list
+    ]
